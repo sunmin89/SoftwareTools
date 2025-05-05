ec2cc054799e8408409fb870854588828b5d8665
sun min <sunmin89@outlook.com>
2025-05-05 08:29:07 +0800

    Switch from strlcpy(BSD style) to strncpy(libc style)

--- lib/src/args.c
+++ lib/src/args.c
@@ -20,6 +20,12 @@ size_t args_getarg(const size_t n, STR const arg,
   size_t index = n + 1;
   if (index < 1 || index > argc)
     return max_size;
-  strlcpy(arg, ARR_AT(argv, index), max_size);
+  // strlcpy was derived from BSD, not compatiable with libc
+  // strlcpy(arg, ARR_AT(argv, index), max_size);
+  // strncpy is more general
+  strncpy(arg, ARR_AT(argv, index), max_size);
+  // should be ended with null
+  arg[max_size - 1] = '\0';
+
   return strnlen(arg, max_size);
 }
